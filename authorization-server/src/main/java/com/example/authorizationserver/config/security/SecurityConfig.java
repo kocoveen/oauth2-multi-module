dc1679690d7fdc17573adab8c0729d602f8739ad
kocoveen <kocoveen@gmail.com>
2024-11-21 18:00:40 +0900

    feat: Client add

--- authorization-server/src/main/java/com/example/authorizationserver/config/security/SecurityConfig.java
+++ authorization-server/src/main/java/com/example/authorizationserver/config/security/SecurityConfig.java
@@ -105,7 +105,7 @@ public class SecurityConfig {
 					t.add(AuthorizationGrantType.REFRESH_TOKEN);
 				})
 				.redirectUris(u -> {
-					u.add("https://www.manning.com/authorized");
+					u.add("http://localhost:8080/login/oauth2/code/my_authorization_server");
 				})
 				.scopes(s -> {
 					s.add(OidcScopes.OPENID);
